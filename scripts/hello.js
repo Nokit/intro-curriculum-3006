855d406e7adfbc96a95dee5f163b93d2aeb4af18
Nokit <stmarc.tn@gmail.com>
2018-11-18 14:18:19 +0900

    assiginment

--- scripts/hello.js
+++ scripts/hello.js
@@ -4,4 +4,11 @@ module.exports = (robot) => {
 		const username = msg.message.user.name;
 		msg.send('Hello, ' + username);
 	});
+        
+        robot.hear(/lot>/i, (msg) => {
+        	const username = msg.message.user.name;
+                const lots = ['youve got a good luck today!','I hope you a good day.','Happiness lives in orninary life', 'Itll be gone tomorrow', 'forget everything that has happend to you today.'];
+                const lot = lots[Math.floor(Math.random() * lots.length)];
+                msg.send(lot + ' : ' + username);
+        });
 };
